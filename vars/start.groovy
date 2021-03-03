73c3a80e9dd5bffe2c32cb849daae12ec332d1bf
Carlos Garcia <carlosgarcia@stratio.com>
2021-03-03 23:17:45 +0100

    Refactor for rocket-2.0

--- vars/start.groovy
+++ vars/start.groovy
@@ -1,21 +1,8 @@
 #!groovy
 
-def call(Map props = [:]) {
+def call(String promomtionParameters) {
 
-    //ansiColor('xterm') {
-
-    if (props["assetType"] == "Workflow") {
-        doWorkflow2envs(props)
-    } else if (props["assetType"] == "MlModel") {
-        doMLModel2envs(props)
-    } else if (props["assetType"] == "MlFlowProject") {
-        doMLProject2envs(props)
-    } else if (props["assetType"] == "AutoMl") {
-        doAutoML2envs(props)
-    } else if (props["assetType"] == "MlTrainer") {
-        doMLTrainer2envs(props)
-    } else {
-        error "Invalid asset type ${props['assetType']}"
+    ansiColor('xterm') {
+        println(promomtionParameters)
     }
-    //}
 }
